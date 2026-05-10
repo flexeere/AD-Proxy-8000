39af87b6fc0fdc542b4ae3f95873b131e8af739e
Arun Kushwaha <149176240+flexeere@users.noreply.github.com>
2026-05-10 18:42:40 +0530

    Update sok-find-os.sh
    24.04 update

--- sok-find-os.sh
+++ sok-find-os.sh
@@ -10,40 +10,91 @@
 # https://flexeere.com/order/contact.php
 ############################################################
 
-if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 24.04" > /dev/null; then
-    echo "ubuntu2404"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 22.04" > /dev/null; then
-    echo "ubuntu2204"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04" > /dev/null; then
-    echo "ubuntu2004"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04" > /dev/null; then
-    echo "ubuntu1804"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04" > /dev/null; then
-    echo "ubuntu1604"
-elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04" > /dev/null; then
-    echo "ubuntu1404"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie" > /dev/null; then
-    echo "debian8"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "stretch" > /dev/null; then
-    echo "debian9"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "buster" > /dev/null; then
-    echo "debian10"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "bullseye" > /dev/null; then
-    echo "debian11"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "bookworm" > /dev/null; then
-    echo "debian12"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "CentOS Linux 7" > /dev/null; then
-    echo "centos7"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "CentOS Linux 8" > /dev/null; then
-    echo "centos8"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "AlmaLinux 8" > /dev/null; then
-    echo "almalinux8"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "AlmaLinux 8" > /dev/null; then
-    echo "almalinux9"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "CentOS Stream 8" > /dev/null; then
-    echo "centos8s"
-elif cat /etc/os-release | grep PRETTY_NAME | grep "CentOS Stream 9" > /dev/null; then
-    echo "centos9"
-else
-    echo "ERROR"
+if [ `whoami` != root ]; then
+	echo "ERROR: You need to run the script as user root or add sudo before command."
+	exit 1
 fi
+
+if [ ! -f /usr/local/bin/sok-find-os ]; then
+    echo "/usr/local/bin/sok-find-os not found"
+    exit 1
+fi
+
+SOK_OS=$(/usr/local/bin/sok-find-os)
+
+if [ $SOK_OS == "ERROR" ]; then
+    echo "OS NOT SUPPORTED.\n"
+    echo "Contact https://flexeere.com/order/contact.php to add support for your OS."
+    exit 1;
+fi
+
+if [ $SOK_OS == "ubuntu2404" ]; then
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu2204" ]; then
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu2004" ]; then
+    /usr/bin/apt -y remove --purge squid*
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1804" ]; then
+    /usr/bin/apt -y remove --purge squid3
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1604" ]; then
+    /usr/bin/apt -y remove --purge squid3
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1404" ]; then
+    /usr/bin/apt remove --purge squid3 -y
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian8" ]; then
+    echo "Uninstalling Squid Proxy on Debian 8"
+    /usr/bin/apt -y remove --purge squid
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian9" ]; then
+    echo "Uninstalling Squid Proxy on Debian 9"
+    /usr/bin/apt -y remove --purge squid
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "debian10" ]; then
+    echo "Uninstalling Squid Proxy on Debian 10"
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "debian11" ]; then
+    echo "Uninstalling Squid Proxy on Debian 11"
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "debian12" ]; then
+    echo "Uninstalling Squid Proxy on Debian 12"
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "centos7" ]; then
+    yum remove squid -y
+    /bin/rm -rf /etc/squid/
+elif [ "$SOK_OS" == "centos8" ] || [ "$SOK_OS" == "almalinux8" ] || [ "$SOK_OS" == "almalinux9" ]; then
+    yum remove squid -y
+    /bin/rm -rf /etc/squid/
+elif [ "$SOK_OS" == "centos8s" ]; then
+    dnf remove squid -y
+    /bin/rm -rf /etc/squid/
+elif [ "$SOK_OS" == "centos9" ]; then
+    dnf remove squid -y
+    /bin/rm -rf /etc/squid/
+fi
+
+rm -f /usr/local/bin/squid-add-user > /dev/null 2>&1
+rm -f /root/squid3-install.sh > /dev/null 2>&1
+rm -f /usr/local/bin/sok-find-os > /dev/null 2>&1
+rm -f /usr/local/bin/squid-uninstall > /dev/null 2>&1
+
+echo 
+echo 
+echo "AD Proxy uninstalled."
+echo "Thank you for using flexeere.com AD proxy installer"
+echo "If you want to reinstall AD Proxy Server, check https://flexeere.com/proxy"
+echo 
